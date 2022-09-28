685d43a53239971b09ef81a5c1e631048759ad84
Ahmed-SD <ahmedadam.abduallah@gmail.com>
2022-09-28 04:11:16 -0700

    v1 release

--- src/components/HomePage/HomeDetails.vue
+++ src/components/HomePage/HomeDetails.vue
@@ -1,43 +1,16 @@
 <template>
   <div class="bg-images rounded">
-    <div
-      class="
-        
-        
-        flex flex-col
-        items-center
-        py-12
-        sm:py-24
-        bg-overlay
-        rounded
-      "
-    >
+    <div class="flex flex-col items-center py-12 sm:py-24 bg-overlay rounded">
       <div class="w-11/12 sm:w-2/3 mb-5 sm:mb-10">
         <h1
-          class="
-            text-2xl
-            sm:text-2xl
-            md:text-3xl
-            lg:text-4xl
-            xl:text-4xl
-            text-center text-white
-            font-extrabold
-            leading-tight
-          "
+          class="text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl text-center text-white font-extrabold leading-tight"
         >
           متجر
           <span class="text-violet-700"> استاناكادو</span>
           يوفر لك افضل المنتجات مع اافضل الاسعار
         </h1>
         <p
-          class="
-            mt-5
-            sm:mt-10
-            text-white
-            font-normal
-            text-center text-lg
-            sm:text-lg
-          "
+          class="mt-5 sm:mt-10 text-white font-normal text-center text-lg sm:text-lg"
         >
           استاناكادو هو محل هدايا يجعل العميل يطلب هديته و هو في المنزل.. و يجعل
           عمليه الشراء و الدفع بسيطه و مريحه للعميل .. و يمكن للعميل تشكيل هديته
@@ -47,46 +20,21 @@
       <div class="flex justify-center items-center">
         <router-link to="/ProductsPage">
           <button
-            class="
-              focus:outline-none
-              bg-indigo-700
-              transition
-              duration-150
-              ease-in-out
-              hover:bg-indigo-600
-              rounded
-              text-white
-              px-4
-              sm:px-10
-              py-2
-              sm:py-4
-              text-sm
-            "
+            class="focus:outline-none bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white px-4 sm:px-10 py-2 sm:py-4 text-sm"
           >
             يمكنك التسوق الان
           </button>
         </router-link>
 
         <button
-          class="
-            mr-4
-            focus:outline-none
-            bg-white
-            transition
-            duration-150
-            ease-in-out
-            hover:border-indigo-600 hover:text-violet-600
-            rounded
-            border border-indigo-700
-            text-violet-700
-            px-4
-            sm:px-10
-            py-2
-            sm:py-4
-            text-sm
-          "
+          class="mr-4 focus:outline-none bg-white transition duration-150 ease-in-out hover:border-indigo-600 hover:text-violet-600 rounded border border-indigo-700 text-violet-700 px-4 sm:px-10 py-2 sm:py-4 text-sm"
         >
-          احصل علي التطبيق
+          <a
+            href="https://play.google.com/store/apps/details?id=com.istanakadostore.app"
+            class="buy-btn"
+          >
+            احصل علي التطبيق
+          </a>
         </button>
       </div>
     </div>
@@ -99,5 +47,8 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.buy-btn {
+  text-decoration: none;
+}
 </style>
